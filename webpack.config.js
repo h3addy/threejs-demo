d9abf2d2fd7dcd5b08998d6a758f36c4ccde60cb
h3addy <hatankaro@gmail.com>
2023-12-04 15:55:50 +0530

    webpack: asset management

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,13 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const toml = require("toml");
+const yaml = require("yamljs");
+const json5 = require("json5");
 
 module.exports = {
-  mode: "development",
-  entry: {
-    index: "./src/index.js",
-    print: "./src/print.js",
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Output Management",
-    }),
-  ],
+  entry: "./src/index.js",
   output: {
-    filename: "[name].bundle.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
-    publicPath: "/",
   },
   module: {
     rules: [
@@ -33,16 +20,38 @@ module.exports = {
         type: "asset/resource",
       },
       {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: "file-loader",
-          },
-        ],
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/i,
+        use: ["xml-loader"],
+      },
+      {
+        test: /\.toml$/i,
+        type: "json",
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: "json",
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/i,
+        type: "json",
+        parser: {
+          parse: json5.parse,
+        },
       },
     ],
-  },
-  optimization: {
-    runtimeChunk: "single",
   },
 };
