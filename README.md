9bc8e1d9fd6e24b7ae82e9707656c25eaab264c8
h3addy <hatankaro@gmail.com>
2023-12-04 16:16:58 +0530

    webpack: development modes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # threejs-demo
- webpack tuts  + threeJS tuts
+
+webpack tuts + threeJS tuts
+
+There are a couple of different options available in webpack that help you automatically compile your code whenever it changes:
+
+1. webpack's Watch Mode - page needs to be refreshed manually
+2. webpack-dev-server - page reloads automatically(HMR)
+3. webpack-dev-middleware - used in webpack-dev-server internally, but configured separately, then page needs to be refreshed manually
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,27 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management",
+      title: "Development",
     }),
   ],
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: "/",
+  },
+  optimization: {
+    runtimeChunk: "single",
   },
 };
