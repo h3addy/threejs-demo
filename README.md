22460877157b26a38fa0049ede53db0a61abd3df
h3addy <hatankaro@gmail.com>
2023-12-05 11:28:17 +0530

    webpack: authoring libraries

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 
 webpack tuts + threeJS tuts
 
-So we're using webpack to bundle our modular application which yields a deployable /dist directory. Once the contents of /dist have been deployed to a server, clients (typically browsers) will hit that server to grab the site and its assets. The last step can be time consuming, which is why browsers use a technique called caching. This allows sites to load faster with less unnecessary network traffic. However, it can also cause headaches when you need new code to be picked up.
-
-Webpack provides a method of templating the filenames using bracketed strings called substitutions. The [contenthash] substitution will add a unique hash based on the content of an asset. When the asset's content changes, [contenthash] will change as well.
+Aside from applications, webpack can also be used to bundle JavaScript libraries
+Using an array as an entry point for a library is not recommended.
--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,49 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  // mode: "development",
   entry: {
     index: "./src/index.js",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Caching",
-    }),
-  ],
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-  },
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     title: "Caching",
+  //   }),
+  // ],
+  // devtool: "inline-source-map",
+  // devServer: {
+  //   static: "./dist",
+  // },
   output: {
-    filename: "[name].[contenthash].js",
+    filename: "webpack-numbers.js",
     path: path.resolve(__dirname, "dist"),
+    globalObject: "this",
+    library: {
+      name: "webpackNumbers",
+      type: "umd",
+    },
     clean: true,
     publicPath: "/",
   },
-  optimization: {
-    moduleIds: "deterministic",
-    runtimeChunk: "single",
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
-        },
-      },
+  externals: {
+    lodash: {
+      commonjs: "lodash",
+      commonjs2: "lodash",
+      amd: "lodash",
+      root: "_",
     },
   },
+  // optimization: {
+  //   moduleIds: "deterministic",
+  //   runtimeChunk: "single",
+  //   splitChunks: {
+  //     cacheGroups: {
+  //       vendor: {
+  //         test: /[\\/]node_modules[\\/]/,
+  //         name: "vendors",
+  //         chunks: "all",
+  //       },
+  //     },
+  //   },
+  // },
 };
