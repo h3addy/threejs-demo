4892662f7d8ae8452a85231fd83baef38bb0398f
h3addy <hatankaro@gmail.com>
2023-12-06 12:34:23 +0530

    webpack: env variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,15 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: {
-    index: "./src/index.js",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Caching",
-    }),
-  ],
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-  },
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-    publicPath: "/",
-  },
-  optimization: {
-    moduleIds: "deterministic",
-    runtimeChunk: "single",
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
-        },
-      },
+module.exports = (env) => {
+  // Use env.<YOUR VARIABLE> here:
+  console.log("Goal: ", env.goal); // 'local'
+  console.log("Production: ", env.production); // true
+
+  return {
+    entry: "./src/index.js",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
-  },
+  };
 };
